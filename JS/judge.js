53f100026ce85241f5a3662faf2790feb9ddbf89
yukkuri <reityan.sakura0104@gmail.com>
2025-10-24 10:23:26 +0900

    ジャッジメントの精度を修正

--- JS/judge.js
+++ JS/judge.js
@@ -2,8 +2,8 @@ const judgementSecIndex = {
     "a": { Cperfect: 0.016, perfect: 0.033, great: 0.066, bad: 0.100 },
     "NOM": { Cperfect: 0.050, perfect: 0.166, great: 0.199, bad: 0.200 },
     "HRD": { Cperfect: 0.050, perfect: 0.099, great: 0.166, bad: 0.200 },
-    "EXP": { Cperfect: 0.025, perfect: 0.049, great: 0.099, bad: 0.166 },
-    "MAS": { Cperfect: 0.016, perfect: 0.033, great: 0.066, bad: 0.133 },
+    "EXP": { Cperfect: 0.025, perfect: 0.050, great: 0.099, bad: 0.166 },
+    "MAS": { Cperfect: 0.025, perfect: 0.050, great: 0.099, bad: 0.166 },
     "REMAS": { Cperfect: 0.016, perfect: 0.033, great: 0.066, bad: 0.133 }
 };
 
@@ -11,7 +11,7 @@ const overJudgementSecIndex = {
     "a": { Cperfect: 0.016, perfect: 0.033, great: 0.066, bad: 0.100 },
     "NOM": { Cperfect: 0.050, perfect: 0.166, great: 0.199, bad: 0.200 },
     "HRD": { Cperfect: 0.050, perfect: 0.099, great: 0.166, bad: 0.200 },
-    "EXP": { Cperfect: 0.025, perfect: 0.049, great: 0.099, bad: 0.166 },
-    "MAS": { Cperfect: 0.016, perfect: 0.033, great: 0.066, bad: 0.133 },
+    "EXP": { Cperfect: 0.025, perfect: 0.050, great: 0.099, bad: 0.166 },
+    "MAS": { Cperfect: 0.025, perfect: 0.050, great: 0.066, bad: 0.166 },
     "REMAS": { Cperfect: 0.016, perfect: 0.033, great: 0.066, bad: 0.133 }
 };
